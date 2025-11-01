6044adc6d8c6a77db1fb1ef4bf1ded4520c8206d
srmp1382 <srmp1382@gmail.com>
2025-11-01 16:58:27 +0530

    pushing once more

--- test_flask_app.py
+++ test_flask_app.py
@@ -1,13 +1,14 @@
 import pytest
 from practice_app import app
-@pytest.fixture()
-def client(app):
+@pytest.fixture
+def client():
     return app.test_client()
-def page_1():
+def test_page_1(client):
     response=client.get('/')
-   
-    assert response.data=='I am in the home page'
-def page_2():
-    response=client.get('/new_page')
+    assert response.status_code==200
+    assert response.data==b'I am in the home page'
 
-    assert response.data=='I am in a new page'
+def test_page_2(client):
+    response=client.get('/new_page')
+    assert response.status_code==200
+    assert response.data==b'I am in a new page'
